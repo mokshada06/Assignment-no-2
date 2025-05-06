a0a0e59952b05941f7e7b2f3c141e2be64684530
mokshada06 <mokshadashpute24@gmail.com>
2025-05-06 14:54:34 +0530

    Add files via upload

---
+++ Assignment no 2.py
@@ -0,0 +1,45 @@
+#3 Task no 1
+
+x=int(input('enter a no: '))
+if (x%2==0):
+    print('the no is even: ',x)
+
+Output :
+enter a no:  6
+the no is even:  6
+
+y=int(input('enter a no: '))
+if (y%2!=0):
+    print('the no is odd: ',y)
+
+Output :
+enter a no:  5
+the no is odd:  5
+
+
+##OR
+a=int(input('enter a no: '))
+if (a%2==0):
+    print('the no is even: ',a)
+else:
+    print('the no is odd: ',a)
+
+Output :
+enter a no:  8
+the no is even:  8
+
+enter a no:  5
+the no is odd:  5
+
+
+#3 Task no 2
+
+sum=0
+for i in range(1,51):
+   sum=sum+i
+print('The of no from 1 to 50 is: ' ,sum)
+
+Output :
+
+
+
